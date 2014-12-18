b9bce648fc3c4d4273d8b9c9e5c6d31c7732d282
Blurredbits <mmorris00@mac.com>
2014-12-18 11:06:45 -0700

    Add locations for lunch

--- lib/coroutine.rb
+++ lib/coroutine.rb
@@ -1,4 +1,4 @@
-require "coroutine/version"
+#require "coroutine/version"
 
 module Coroutine
 
@@ -7,7 +7,19 @@ module Coroutine
     days = %w[mon tue wed thu fri]
 
     days.map do |day|
-      "#{day} => #{employees.sample(2)}"
+      who_is_going = employees.sample(2);
+      "#{day} => #{who_is_going} @ #{where_will_we_have_lunch?(who_is_going)}"
     end
   end
+
+  def self.where_will_we_have_lunch?(employees)
+    restaurants = %w[sherpas reubens jaipur japango kasa zomama khow-thai illegalpetes kitchennextdoor mediterranean leaf lyfe falafelking lindseys taco yellowdeli wokeat]
+
+    if employees.include?("mark")
+      restaurants = "jaipur"
+    else
+      restaurants.sample
+    end
+  end
+
 end
